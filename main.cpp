b7c9cf3db8c18ca94641f3985135609270afb0a1
Yaseungho <molkanglee@gmail.com>
2024-05-21 14:32:21 +0900

    test subwindow

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
-#include <iostream>
 #include <ncurses.h>
-using namespace std;
-
 int main()
 {
     initscr();
-    printw("Hello World !!!");
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_WHITE);
+    box(stdscr, 0, 0);
+    attron(COLOR_PAIR(1));
+    wbkgd(stdscr, COLOR_PAIR(1)); // 윈도우 백그라운드 컬러
+    mvprintw(1,1,"Hello NCURSES!");
+    // 기본 윈도우 내의 서브윈도우 생성
+    WINDOW *win = subwin(stdscr, 5, 60, 10, 10);
+    init_pair(2, COLOR_BLACK, COLOR_BLUE); // 윈도우 백그라운드 컬러
+    box(win, 0, 0);
+    attron(COLOR_PAIR(2));
+    wbkgd(win, COLOR_PAIR(2));
+    mvwprintw(win,1,1, "Hello SubWindow!");
     refresh();
     getch();
+    delwin(win);
     endwin();
     return 0;
 }
